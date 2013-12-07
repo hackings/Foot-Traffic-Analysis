8fe5de267bb91c7088ca7a6c0e5b0aadf519c2c7
Vishnu Atrai <me@vishnuatrai.com>
2013-12-07 19:33:02 +0530

    line seperator between tests

--- foot_traffic_analysis.rb
+++ foot_traffic_analysis.rb
@@ -1,7 +1,9 @@
 Dir['./lib/**/*.rb'].each{|file| require file }
 
+puts '---------------Sample test 1--------------------'
 sample_1_analysis = FootTrafficAnalysis.new('sample_1.txt')
 sample_1_analysis.report
 
+puts '---------------Sample test 2--------------------'
 sample_2_analysis = FootTrafficAnalysis.new('sample_2.txt')
 sample_2_analysis.report
